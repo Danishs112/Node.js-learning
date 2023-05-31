8a4f19536dec12ae0bacb45c5b020f7b15eedf4c
Danish Soma <danish.soma@wallstreetenglish.com>
2023-05-31 18:08:55 +0530

    added eslint prettier and dotenv file

--- app.js
+++ app.js
@@ -1,33 +1,13 @@
-const express = require("express");
-const morgan = require("morgan");
-
-
-
-
-const tourRouter = require('./routes/tourRouter')
-const userRouter = require('./routes/userRouter')
-
+const express = require('express');
+const tourRouter = require('./routes/tourRouter');
+const userRouter = require('./routes/userRouter');
 
 const app = express();
 
 // app.use(morgan("short"));
 
-
-app.use(express.json({ strict: false }))
-
-// app.use((req, res, next) => {
-//   // console.log("hello from middleware")
-//   next();
-// });
-
-// app.use((req, res, next) => {
-//   req.requestTime = new Date().toISOString();
-//   next();
-// });
-
-app.use("/api/v1/tours",tourRouter);
-app.use("/api/v1/users",userRouter)
-
+app.use(express.json({ strict: false }));
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
-
